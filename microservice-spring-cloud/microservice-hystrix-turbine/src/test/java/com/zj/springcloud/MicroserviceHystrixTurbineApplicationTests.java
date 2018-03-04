88a8a5955de489f08c5c54890326e102bd1e780a
wuzejian <aidaowuqiong@163.com>
2018-03-04 22:40:40 +0800

    2018.3.4

--- microservice-spring-cloud/microservice-hystrix-turbine/src/test/java/com/zj/springcloud/MicroserviceHystrixTurbineApplicationTests.java
+++ microservice-spring-cloud/microservice-hystrix-turbine/src/test/java/com/zj/springcloud/MicroserviceHystrixTurbineApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroserviceHystrixDashBoardApplicationTests {
+public class MicroserviceHystrixTurbineApplicationTests {
 
 	@Test
 	public void contextLoads() {
